51d6d052707da34b791b2932ae2c3b3a956a7093
José Carneiro <josemcarneiro@gmail.com>
2020-07-20 16:34:47 +0100

    Adds express server

---
+++ node-express-introduction/index.js
@@ -0,0 +1,35 @@
+// 1 - "npm init" (or "npm init -y")
+// 2 - "npm install express" (or "npm i express")
+const express = require('express');
+
+const app = express();
+
+// Serve css and other static files from directory "public"
+// express.static() returns middleware that handles every request,
+// and for whatever request matches a static file,
+// it sends the static file
+app.use(express.static('public'));
+
+// app.get('/style.css', (request, response) => {
+//   response.sendFile(__dirname + '/views/style.css');
+// });
+
+// Set route handlers for common endpoints
+app.get('/jose', (request, response) => {
+  // response.send('Olá mundo');
+  response.sendFile(__dirname + '/views/jose.html');
+});
+
+app.get('/santi', (request, response) => {
+  response.send('Hola mundo');
+});
+
+app.get('/aline', (request, response) => {
+  response.send('Oi mundo');
+});
+
+app.get('*', (request, response) => {
+  response.send('Hello World');
+});
+
+app.listen(3000);
